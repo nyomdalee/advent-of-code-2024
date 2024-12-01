bcbb02fa3d12bff19102a6080f385b06e2dfc67f
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-01 14:21:37 +0100

    01-01 solution

--- 01 (Historian Hysteria)/Program.cs
+++ 01 (Historian Hysteria)/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+namespace One;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        var inputLines = File.ReadAllLines("input.txt");
+        var sampleLines = File.ReadAllLines("sample.txt");
+        var parsedResult = long.TryParse(File.ReadAllLines("sampleResult.txt").First(), out long sampleResult);
+
+        if (!parsedResult)
+        {
+            Console.WriteLine("Could not read sample result.");
+            return;
+        }
+
+        var sw = new Stopwatch();
+        sw.Start();
+
+        var actualSampleResult = Run(sampleLines);
+        if (actualSampleResult != sampleResult)
+        {
+            Console.WriteLine("Sample result does not match, terminating.");
+            return;
+        }
+
+        sw.Stop();
+        Console.WriteLine($"Sample result confirmed in {sw.ElapsedMilliseconds} ms.");
+
+        sw.Restart();
+        var mainResult = Run(inputLines);
+        sw.Stop();
+        Console.WriteLine($"Main run finished in: {sw.ElapsedMilliseconds} ms.");
+        Console.WriteLine("Result:");
+        Console.WriteLine(mainResult);
+        Clipboard.SetText(mainResult.ToString());
+        Console.WriteLine("Copied to clipboard.");
+    }
+
+    private static long Run(string[] lines)
+    {
+        List<long> left = [];
+        List<long> right = [];
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            left.Add(int.Parse(parts[0]));
+            right.Add(int.Parse(parts[1]));
+        }
+
+        left = [.. left.Order()];
+        right = [.. right.Order()];
+
+        var combined = left.Zip(right);
+        var result = combined.Aggregate(0, (long total, (long First, long Second) next) => total + Math.Abs(next.First - next.Second));
+        return result;
+    }
+}
