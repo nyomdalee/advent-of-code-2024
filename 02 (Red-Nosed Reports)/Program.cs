762a4edccf4f3a0cc8ce21412ecc0abf400b2547
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-02 13:37:57 +0100

    02-02, really bad solution

--- 02 (Red-Nosed Reports)/Program.cs
+++ 02 (Red-Nosed Reports)/Program.cs
@@ -12,28 +12,46 @@ public static class Program
 
     private static long Run(string[] lines)
     {
-        List<List<long>> diffs = [];
-        foreach (var line in lines)
-        {
-            long[] split = Array.ConvertAll(line.Split(' '), long.Parse);
-
-            List<long> diffPart = [];
-            for (int i = 1; i < split.Length; i++)
-            {
-                diffPart.Add(split[i - 1] - split[i]);
-            }
-            diffs.Add(diffPart);
-        }
+        var numberLines = lines.Select(x => Array.ConvertAll(x.Split(' '), long.Parse)).ToList();
 
         long safeCount = 0;
-        foreach (var line in diffs)
+
+        foreach (var line in numberLines)
         {
-            if (line.All(x => x > 0 && x <= 3) || line.All(x => x < 0 && x >= -3))
+            if (BruteForce(line))
             {
                 safeCount++;
             }
         }
-
         return safeCount;
+    }
+    private static bool BruteForce(long[] line)
+    {
+        List<long[]> exhaustiveLines = [];
+        exhaustiveLines.Add(line);
+        for (var i = 0; i < line.Length; i++)
+        {
+            long[] lineCopy = new long[line.Length];
+            line.CopyTo(lineCopy, 0);
+            var listLine = lineCopy.ToList();
+            listLine.RemoveAt(i);
+            exhaustiveLines.Add(listLine.ToArray());
+        }
+
+        foreach (var exhaustiveLine in exhaustiveLines)
+        {
+            List<long> diffs = [];
+            for (int i = 1; i < exhaustiveLine.Length; i++)
+            {
+                diffs.Add(exhaustiveLine[i - 1] - exhaustiveLine[i]);
+            }
+
+            if (diffs.All(x => x > 0 && x <= 3) || diffs.All(x => x < 0 && x >= -3))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
