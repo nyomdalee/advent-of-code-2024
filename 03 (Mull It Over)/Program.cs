75876faf667c2d9736697672736d0e0836ea0ebf
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-03 10:57:40 +0100

    03-01 solution

---
+++ 03 (Mull It Over)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using Utils;
+
+namespace Two;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Solver.Solve(Run);
+    }
+
+    private static long Run(string? text)
+    {
+        var matches = Regex.Matches(text ?? throw new Exception("null input"), @"mul\((\d{1,3}),(\d{1,3})\)");
+        var what = matches[0].Groups[1].Value;
+
+        return matches.Aggregate(0L, (current, match) =>
+            current + (long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value)));
+    }
+}
--- Utils/Solver.cs
+++ Utils/Solver.cs
@@ -18,7 +18,52 @@ public static class Solver
 
         var inputLines = File.ReadAllLines(inputPath);
         var sampleLines = File.ReadAllLines(samplePath);
-        var parsedResult = long.TryParse(File.ReadAllLines(sampleResultPath).First(), out long sampleResult);
+        var parsedResult = long.TryParse(File.ReadAllText(sampleResultPath), out long sampleResult);
+
+        if (inputLines.Length == 0 || sampleLines.Length == 0 || !parsedResult)
+        {
+            Console.WriteLine("Invalid inputs.");
+            return;
+        }
+
+        var sw = new Stopwatch();
+
+        sw.Start();
+        var actualSampleResult = solveFunc(sampleLines);
+        if (actualSampleResult != sampleResult)
+        {
+            Console.WriteLine($"Provided result: {sampleResult}, computed result: {actualSampleResult}.");
+            Console.WriteLine("Sample result does not match, terminating.");
+            return;
+        }
+        sw.Stop();
+        Console.WriteLine($"Sample result confirmed in {sw.ElapsedMilliseconds} ms.");
+
+        sw.Restart();
+        var mainResult = solveFunc(inputLines);
+        sw.Stop();
+
+        Console.WriteLine($"Main run finished in: {sw.ElapsedMilliseconds} ms.");
+        Console.WriteLine("Result:");
+        Console.WriteLine(mainResult);
+        Clipboard.SetText(mainResult.ToString());
+        Console.WriteLine("Result copied to clipboard.");
+    }
+
+    public static void Solve(
+        Func<string?, long> solveFunc,
+        string inputFileName = "input.txt",
+        string sampleFileName = "sample.txt",
+        string sampleResultFileName = "sampleResult.txt")
+    {
+        string basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? AppContext.BaseDirectory;
+        string inputPath = Path.Combine(basePath, inputFileName);
+        string samplePath = Path.Combine(basePath, sampleFileName);
+        string sampleResultPath = Path.Combine(basePath, sampleResultFileName);
+
+        var inputLines = File.ReadAllText(inputPath);
+        var sampleLines = File.ReadAllText(samplePath);
+        var parsedResult = long.TryParse(File.ReadAllText(sampleResultPath), out long sampleResult);
 
         if (inputLines.Length == 0 || sampleLines.Length == 0 || !parsedResult)
         {
