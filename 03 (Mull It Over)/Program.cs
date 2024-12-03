147e6dd112306d30e91fa527551d45f7b4c8af45
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-03 12:42:05 +0100

    03-02 solution

--- 03 (Mull It Over)/Program.cs
+++ 03 (Mull It Over)/Program.cs
@@ -13,10 +13,8 @@ public static class Program
 
     private static long Run(string? text)
     {
-        var matches = Regex.Matches(text ?? throw new Exception("null input"), @"mul\((\d{1,3}),(\d{1,3})\)");
-        var what = matches[0].Groups[1].Value;
-
-        return matches.Aggregate(0L, (current, match) =>
-            current + (long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value)));
+        var relevant = Regex.Matches(text ?? throw new Exception("null input"), @"(?:^|do\(\)).*?(?:don't\(\)|$)", RegexOptions.Multiline);
+        var matches = relevant.SelectMany(x => Regex.Matches(x.Value, @"mul\((\d{1,3}),(\d{1,3})\)"));
+        return matches.Aggregate(0L, (current, match) => current + (long.Parse(match.Groups[1].Value) * long.Parse(match.Groups[2].Value)));
     }
 }
