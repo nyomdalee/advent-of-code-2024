1efb75478a6ee031c6ee7e853a0b67d05db5912b
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-05 19:35:27 +0100

    05-01 solution

---
+++ 05 (Print Queue)/Program.cs
@@ -0,0 +1,47 @@
+﻿using Utils;
+
+namespace Five;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Solver.Solve<string[]>(Run);
+    }
+
+    private static long Run(string[]? lines)
+    {
+        var rules = lines
+            .Where(x => x.Contains('|'))
+            .Select(x => x.Split('|'))
+            .Select(x => (First: int.Parse(x[0]), Second: int.Parse(x[1])))
+            .ToLookup(x => x.Second, x => x.First);
+
+        var updates = lines
+            .Where(x => x.Contains(','))
+            .Select(x => x.Split(","))
+            .Select(x => x.ToList().ConvertAll(x => int.Parse(x)).ToArray())
+            .ToArray();
+
+        return updates
+            .Where(update => IsValid(update, rules))
+            .Sum(update => (long)update[update.Length / 2]);
+    }
+
+    private static bool IsValid(int[] update, ILookup<int, int> rules)
+    {
+        for (var i = 0; i < update.Length; i++)
+        {
+            var current = update[i];
+            foreach (var r in rules[current])
+            {
+                if (update.Contains(r) && Array.IndexOf(update, r) >= i)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
