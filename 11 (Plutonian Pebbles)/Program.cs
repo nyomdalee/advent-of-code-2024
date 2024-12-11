ae4caeaea4df312e4952de75bcb123e66be5be87
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-11 14:58:06 +0100

    11-02 solution

--- 11 (Plutonian Pebbles)/Program.cs
+++ 11 (Plutonian Pebbles)/Program.cs
@@ -7,42 +7,54 @@ public static class Program
     [STAThread]
     public static void Main()
     {
-        Solver.Solve<string>(Run);
+        Solver.Solve<string>(Run, true);
     }
 
     private static long Run(string? line)
     {
         List<long> rocks = line.Split(' ').ToList().ConvertAll(long.Parse);
+        Dictionary<long, long> rockDict = rocks.GroupBy(x => x).ToDictionary(x => x.Key, x => (long)x.Count());
 
-        for (long i = 0; i < 25; i++)
+        for (long i = 0; i < 75; i++)
         {
-            List<long> newRocks = [];
-            foreach (var rock in rocks)
+            Dictionary<long, long> newRocks = [];
+            foreach (var rock in rockDict)
             {
                 HandleRock(rock, newRocks);
             }
-            rocks = newRocks;
+            rockDict = newRocks;
         }
-
-        return rocks.Count;
+        return rockDict.Sum(x => x.Value);
     }
 
-    private static void HandleRock(long rock, List<long> newRocks)
+    private static void HandleRock(KeyValuePair<long, long> rock, Dictionary<long, long> newRocks)
     {
-        if (rock == 0)
+        void AddOrUpdate(long key, long value)
         {
-            newRocks.Add(1);
+            if (newRocks.ContainsKey(key))
+            {
+                newRocks[key] += value;
+            }
+            else
+            {
+                newRocks[key] = value;
+            }
         }
-        else if ((rock.ToString().Length % 2) == 0)
+
+        if (rock.Key == 0)
         {
-            var rockstring = rock.ToString();
-            var half = rockstring.Length / 2;
-            newRocks.Add(long.Parse(rockstring[..half]));
-            newRocks.Add(long.Parse(rockstring[half..]));
+            AddOrUpdate(1, rock.Value);
+        }
+        else if ((rock.Key.ToString().Length % 2) == 0)
+        {
+            int half = rock.Key.ToString().Length / 2;
+
+            AddOrUpdate(long.Parse(rock.Key.ToString()[..half]), rock.Value);
+            AddOrUpdate(long.Parse(rock.Key.ToString()[half..]), rock.Value);
         }
         else
         {
-            newRocks.Add(rock * 2024);
+            AddOrUpdate(rock.Key * 2024, rock.Value);
         }
     }
 }
