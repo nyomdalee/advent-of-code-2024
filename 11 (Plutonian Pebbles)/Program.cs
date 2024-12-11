74f6457176e785286fc05e1d89ff4755fe3aa9a8
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-11 14:04:42 +0100

    11-01 solution

---
+++ 11 (Plutonian Pebbles)/Program.cs
@@ -0,0 +1,48 @@
+﻿using Utils;
+
+namespace Eleven;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Solver.Solve<string>(Run);
+    }
+
+    private static long Run(string? line)
+    {
+        List<long> rocks = line.Split(' ').ToList().ConvertAll(long.Parse);
+
+        for (long i = 0; i < 25; i++)
+        {
+            List<long> newRocks = [];
+            foreach (var rock in rocks)
+            {
+                HandleRock(rock, newRocks);
+            }
+            rocks = newRocks;
+        }
+
+        return rocks.Count;
+    }
+
+    private static void HandleRock(long rock, List<long> newRocks)
+    {
+        if (rock == 0)
+        {
+            newRocks.Add(1);
+        }
+        else if ((rock.ToString().Length % 2) == 0)
+        {
+            var rockstring = rock.ToString();
+            var half = rockstring.Length / 2;
+            newRocks.Add(long.Parse(rockstring[..half]));
+            newRocks.Add(long.Parse(rockstring[half..]));
+        }
+        else
+        {
+            newRocks.Add(rock * 2024);
+        }
+    }
+}
