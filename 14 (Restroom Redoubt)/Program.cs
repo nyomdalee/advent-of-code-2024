c2df4fc2728db75ceaa53d7f1b1ce93cc74d2f7c
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-14 16:38:54 +0100

    14-02 solution

--- 14 (Restroom Redoubt)/Program.cs
+++ 14 (Restroom Redoubt)/Program.cs
@@ -25,35 +25,31 @@ public static class Program
                 new Velocity(int.Parse(match.Groups["Vx"].Value), int.Parse(match.Groups["Vy"].Value))))
             .ToList();
 
-        var robitsAfter = robits.Select(RobitPositionAfter).ToList();
-
-        var quads = robitsAfter.Select(GetQuadrant).ToList();
-
-        var result = quads
-            .GroupBy(x => x)
-            .Where(x => x.Key != 0)
-            .Aggregate(1, (current, next) => current * next.Count());
-        return result;
-    }
-
-    private static void DebugPrint(List<Robit> robots)
-    {
-        for (int j = 0; j <= YWidth - 1; j++)
+        long lowestDanger = long.MaxValue;
+        int ay = 0;
+        for (int i = 1; i <= XWidth * YWidth; i++)
         {
-            for (int i = 0; i <= XWidth - 1; i++)
+            robits = robits.ConvertAll(RobitPositionAfter);
+            var quads = robits.ConvertAll(GetQuadrant);
+            var danger = quads
+                .GroupBy(x => x)
+                .Where(x => x.Key != 0)
+                .Aggregate(1, (current, next) => current * next.Count());
+
+            if (danger < lowestDanger)
             {
-                var c = robots.Count(x => x.Point.X == i && x.Point.Y == j);
-                var wot = c > 0 ? (char)c : '.';
-                Console.Write(c);
+                lowestDanger = danger;
+                ay = i;
             }
-            Console.WriteLine();
         }
+
+        return ay;
     }
 
-    private static Point RobitPositionAfter(Robit robit)
+    private static Robit RobitPositionAfter(Robit robit)
     {
-        int x = (robit.Point.X + (robit.Velocity.X * Seconds)) % XWidth;
-        int y = (robit.Point.Y + (robit.Velocity.Y * Seconds)) % YWidth;
+        int x = (robit.Point.X + robit.Velocity.X) % XWidth;
+        int y = (robit.Point.Y + robit.Velocity.Y) % YWidth;
 
         if (y < 0)
         {
@@ -64,30 +60,30 @@ public static class Program
             x += XWidth;
         }
 
-        return new Point(x, y);
+        return robit with { Point = new Point(x, y) };
     }
 
-    private static long GetQuadrant(Point point)
+    private static long GetQuadrant(Robit robit)
     {
-        if (point.X < XWidth / 2)
+        if (robit.Point.X < XWidth / 2)
         {
-            if (point.Y < YWidth / 2)
+            if (robit.Point.Y < YWidth / 2)
             {
                 return 1;
             }
-            else if (point.Y > YWidth / 2)
+            else if (robit.Point.Y > YWidth / 2)
             {
                 return 2;
             }
         }
 
-        if (point.X > XWidth / 2)
+        if (robit.Point.X > XWidth / 2)
         {
-            if (point.Y < YWidth / 2)
+            if (robit.Point.Y < YWidth / 2)
             {
                 return 3;
             }
-            else if (point.Y > YWidth / 2)
+            else if (robit.Point.Y > YWidth / 2)
             {
                 return 4;
             }
