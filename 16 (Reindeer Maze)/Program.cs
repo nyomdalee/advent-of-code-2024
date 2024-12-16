eb0303cc72e25c62b50f88b15c386c4d7be30109
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-16 19:50:21 +0100

    16-02 solution

--- 16 (Reindeer Maze)/Program.cs
+++ 16 (Reindeer Maze)/Program.cs
@@ -8,26 +8,10 @@ public static class Program
     private const char Wall = '#';
     private const char End = 'E';
 
-    private static readonly Dictionary<DirectionName, Direction> turnRight = new()
-    {
-        { DirectionName.North, Grid.GetDirectionByName(DirectionName.East) },
-        { DirectionName.East, Grid.GetDirectionByName(DirectionName.South) },
-        { DirectionName.South, Grid.GetDirectionByName(DirectionName.West) },
-        { DirectionName.West, Grid.GetDirectionByName(DirectionName.North) },
-    };
-
-    private static readonly Dictionary<DirectionName, Direction> turnLeft = new()
-    {
-        { DirectionName.North, Grid.GetDirectionByName(DirectionName.West) },
-        { DirectionName.East, Grid.GetDirectionByName(DirectionName.North) },
-        { DirectionName.South, Grid.GetDirectionByName(DirectionName.East) },
-        { DirectionName.West, Grid.GetDirectionByName(DirectionName.South) },
-    };
-
     [STAThread]
     public static void Main()
     {
-        Solver.Solve<string[]>(Run);
+        Solver.Solve<string[]>(Run, true);
     }
 
     private static long Run(string[]? lines)
@@ -39,32 +23,94 @@ public static class Program
         var startDirection = Grid.GetDirectionByName(DirectionName.East);
 
         long[,,] lowestVisited = new long[grid.UpperBound.X + 1, grid.UpperBound.Y + 1, 4];
-
-        Queue<(Point Point, Direction Direction, long Cost)> toExpand = new([(start, startDirection, 0)]);
+        var startT = (start, startDirection, 0);
+        Queue<(Point Point, Direction Direction, long Cost)> toExpand = new([startT]);
         long lowestEnd = long.MaxValue;
-
+        lowestVisited[start.X, start.Y, (int)startDirection.Name] = 0;
         while (toExpand.Count > 0)
         {
             var next = toExpand.Dequeue();
 
             TryExpand(grid, next, next.Direction, next.Cost + 1, lowestVisited, toExpand, ref lowestEnd);
-            TryExpand(grid, next, turnRight[next.Direction.Name], next.Cost + 1001, lowestVisited, toExpand, ref lowestEnd);
-            TryExpand(grid, next, turnLeft[next.Direction.Name], next.Cost + 1001, lowestVisited, toExpand, ref lowestEnd);
+            TryExpand(grid, next, Grid.TurnRight[next.Direction.Name], next.Cost + 1001, lowestVisited, toExpand, ref lowestEnd);
+            TryExpand(grid, next, Grid.TurnLeft[next.Direction.Name], next.Cost + 1001, lowestVisited, toExpand, ref lowestEnd);
         }
 
-        return lowestEnd;
+        List<(Point Point, Direction Direction, long Cost)> ends = [];
+        foreach (var dir in Grid.CardinalDirections)
+        {
+            if (lowestVisited[end.X, end.Y, (int)dir.Name] == lowestEnd)
+            {
+                ends.Add((new(end.X, end.Y), dir, lowestVisited[end.X, end.Y, (int)dir.Name]));
+            }
+        }
+
+        List<Point> pois = [];
+        foreach (var validEnd in ends)
+        {
+            pois = pois.Union(FindPath(validEnd, lowestVisited)).ToList();
+        }
+
+        return pois.Count + 1;
+    }
+
+    private static List<Point> FindPath((Point Point, Direction Direction, long Cost) end, long[,,] lowestVisited)
+    {
+        List<Point> pathPoints = [];
+
+        List<List<(Point Point, Direction Direction, long Cost)>> currentLayer = [[end]];
+
+        while (currentLayer.Count > 0)
+        {
+            List<List<(Point Point, Direction Direction, long Cost)>> nextLayer = [];
+            foreach (var path in currentLayer)
+            {
+                var next = path.Last();
+
+                var pointToCheck = Grid.Step(next.Point, next.Direction, -1);
+                var checkDirect = next.Direction;
+                var checkleft = Grid.TurnLeft[next.Direction.Name];
+                var checkright = Grid.TurnRight[next.Direction.Name];
+
+                foreach (var dir in (List<(Direction Direction, long Decrement)>)([(checkDirect, -1), (checkleft, -1001), (checkright, -1001)]))
+                {
+                    var dirValue = lowestVisited[pointToCheck.X, pointToCheck.Y, (int)dir.Direction.Name];
+                    var expectedValue = next.Cost + dir.Decrement;
+                    if (lowestVisited[pointToCheck.X, pointToCheck.Y, (int)dir.Direction.Name] == next.Cost + dir.Decrement)
+                    {
+                        if (next.Cost + dir.Decrement == 0)
+                        {
+                            pathPoints.AddRange(path.ConvertAll(x => x.Point));
+                        }
+                        else
+                        {
+                            nextLayer.Add([.. path, (pointToCheck, dir.Direction, next.Cost + dir.Decrement)]);
+                        }
+                    }
+                }
+            }
+            currentLayer = nextLayer;
+        }
+        return pathPoints;
     }
 
     private static void TryExpand(Grid grid, (Point Point, Direction Direction, long Cost) next, Direction direction, long newCost, long[,,] lowestVisited, Queue<(Point, Direction, long)> toExpand, ref long lowestEnd)
     {
-        if (newCost >= lowestEnd)
+        if (newCost > lowestEnd)
         {
             return;
         }
 
-        var newValue = grid.GetValueAfterMove(next.Point, direction);
+        var pointToEval = Grid.Step(next.Point, direction);
+        var newValue = grid.GetValue(pointToEval);
+
         if (Equals(newValue, End))
         {
+            if (lowestVisited[pointToEval.X, pointToEval.Y, (int)direction.Name] == 0 || lowestVisited[pointToEval.X, pointToEval.Y, (int)direction.Name] > newCost)
+            {
+                lowestVisited[pointToEval.X, pointToEval.Y + direction.Y, (int)direction.Name] = newCost;
+            }
+
             if (newCost < lowestEnd)
             {
                 lowestEnd = newCost;
@@ -74,10 +120,10 @@ public static class Program
 
         if (!Equals(newValue, Wall))
         {
-            if (lowestVisited[next.Point.X, next.Point.Y, (int)direction.Name] == 0 || lowestVisited[next.Point.X, next.Point.Y, (int)direction.Name] > newCost)
+            if (lowestVisited[pointToEval.X, pointToEval.Y, (int)direction.Name] == 0 || lowestVisited[pointToEval.X, pointToEval.Y, (int)direction.Name] > newCost)
             {
-                lowestVisited[next.Point.X, next.Point.Y, (int)direction.Name] = newCost;
-                toExpand.Enqueue((Grid.Step(next.Point, direction), direction, newCost));
+                lowestVisited[pointToEval.X, pointToEval.Y, (int)direction.Name] = newCost;
+                toExpand.Enqueue((pointToEval, direction, newCost));
             }
         }
     }
