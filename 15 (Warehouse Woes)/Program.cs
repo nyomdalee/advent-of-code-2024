47d4868b6ffc1585812ad64b99f3160b04d4ba51
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-16 02:22:02 +0100

    15-02 disgusting solution

--- 15 (Warehouse Woes)/Program.cs
+++ 15 (Warehouse Woes)/Program.cs
@@ -7,6 +7,8 @@ public static class Program
 {
     private const char Robot = '@';
     private const char Box = 'O';
+    private const char BoxLeft = '[';
+    private const char BoxRight = ']';
     private const char Wall = '#';
     private const char Empty = '.';
 
@@ -28,13 +30,23 @@ public static class Program
     {
         var yek = lines.Split("\r\n\r\n");
 
-        var e = yek[0].Split("\r\n").ToList();
-        e.Reverse();
-        var grid = Grid.FromLines(e.ToArray());
+        var gridLines = yek[0].Split("\r\n").ToList();
+        List<string> expandedLines = [];
+        foreach (var gridLine in gridLines)
+        {
+            var e = gridLine;
+            e = e.Replace("#", "##");
+            e = e.Replace("O", "[]");
+            e = e.Replace(".", "..");
+            e = e.Replace("@", "@.");
+            expandedLines.Add(e);
+        }
+        expandedLines.Reverse();
+        var grid = Grid.FromLines(expandedLines.ToArray());
 
         var distr = yek[1];
         distr = distr.Replace(Environment.NewLine, "");
-        List<Direction> moves = new List<Direction>();
+        List<Direction> moves = [];
         foreach (var chara in distr)
         {
             moves.Add(dirTranslate[chara]);
@@ -43,8 +55,8 @@ public static class Program
         var robitPosition = grid.GetPointsByPredicate(x => Equals(x, Robot)).First();
         PlaySokoban(grid, robitPosition, moves);
 
-        var boxes = grid.GetPointsByPredicate(x => Equals(x, Box)).ToList();
-        return boxes.Aggregate(0, (current, next) => current + (next.Y * 100) + (next.X));
+        var boxes = grid.GetPointsByPredicate(x => Equals(x, BoxLeft)).ToList();
+        return boxes.Sum(x => (x.Y * 100) + x.X);
     }
 
     private static void PlaySokoban(Grid grid, Point robit, List<Direction> moves)
@@ -55,26 +67,85 @@ public static class Program
 
             if (Equals(valueAfter, Empty))
             {
-                grid.SetValue(robit, Empty);
-                robit = Grid.Step(robit, move);
-                grid.SetValue(robit, Robot);
+                robit = MoveRobit(grid, robit, move);
             }
 
-            if (Equals(valueAfter, Box) && TryGetPushLength(grid, robit, move, out var length))
+            if (Equals(valueAfter, BoxLeft) || Equals(valueAfter, BoxRight))
             {
-                Push(grid, robit, move, length);
-                robit = Grid.Step(robit, move);
+                if (move.Name is DirectionName.East or DirectionName.West)
+                {
+                    if (TryPushHorizontal(grid, robit, move))
+                    {
+                        robit = MoveRobit(grid, robit, move);
+                    }
+                }
+                else
+                {
+                    if (TryPushVertical(grid, robit, move))
+                    {
+                        robit = MoveRobit(grid, robit, move);
+                    }
+                }
             }
         }
     }
 
-    private static bool TryGetPushLength(Grid grid, Point robit, Direction move, out int length)
+    private static bool TryPushVertical(Grid grid, Point robit, Direction move)
     {
-        length = 1;
+        var currentLayer = ExpandBox(grid, Grid.Step(robit, move));
+        List<Point> allToMove = [];
+
+        while (currentLayer.Count > 0)
+        {
+            allToMove.AddRange(currentLayer);
+            List<Point> nextLayer = [];
+
+            foreach (var piece in currentLayer)
+            {
+                var value = grid.GetValueAfterMove(piece, move);
+
+                if (Equals(value, Wall))
+                {
+                    return false;
+                }
+
+                if (Equals(value, BoxLeft) || Equals(value, BoxRight))
+                {
+                    nextLayer.AddRange(ExpandBox(grid, Grid.Step(piece, move)));
+                }
+            }
+
+            currentLayer = nextLayer;
+        }
+        PushBoxesVertical(grid, allToMove, move);
+        return true;
+    }
+
+    private static void PushBoxesVertical(Grid grid, List<Point> boxes, Direction move)
+    {
+        var toMove = boxes.ConvertAll(x => (Point: x, Value: grid.GetValue(x)));
+        List<Point> newPositions = [];
+        foreach (var box in toMove)
+        {
+            var nP = Grid.Step(box.Point, move);
+            newPositions.Add(nP);
+            grid.SetValue(nP, box.Value);
+        }
+
+        var newEmpty = boxes.Except(newPositions).ToList();
+        foreach (var empty in newEmpty)
+        {
+            grid.SetValue(empty, Empty);
+        }
+    }
+
+    private static bool TryPushHorizontal(Grid grid, Point robit, Direction move)
+    {
+        var boxes = 1;
         while (true)
         {
-            length++;
-            var value = grid.GetValueAfterMove(robit, move, length);
+            boxes += 2;
+            var value = grid.GetValueAfterMove(robit, move, boxes);
 
             if (Equals(value, Wall))
             {
@@ -83,17 +154,31 @@ public static class Program
 
             if (Equals(value, Empty))
             {
+                PushBoxesHorizontal(grid, robit, move, boxes);
                 return true;
             }
         }
     }
 
-    private static void Push(Grid grid, Point robit, Direction move, int length)
+    private static void PushBoxesHorizontal(Grid grid, Point robit, Direction move, int boxes)
     {
-        grid.SetValue(robit, Empty);
-        for (var i = 1; i <= length; i++)
+        var first = move.X == -1 ? BoxLeft : BoxRight;
+        var second = move.X == -1 ? BoxRight : BoxLeft;
+
+        for (var i = 0; i <= boxes; i++)
         {
-            grid.SetValueAfterMove(robit, move, i == 1 ? Robot : Box, i);
+            grid.SetValueAfterMove(robit, move, i % 2 == 1 ? first : second, i);
         }
     }
+
+    private static Point MoveRobit(Grid grid, Point robit, Direction move)
+    {
+        grid.SetValue(robit, Empty);
+        robit = Grid.Step(robit, move);
+        grid.SetValue(robit, Robot);
+        return robit;
+    }
+
+    private static List<Point> ExpandBox(Grid grid, Point boxPart) =>
+        [boxPart, new(boxPart.X + (Equals(grid.GetValue(boxPart), BoxLeft) ? 1 : -1), boxPart.Y)];
 }
