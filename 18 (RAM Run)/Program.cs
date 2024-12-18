a165c1c9d61ff199f13fe51956f88a07c714c979
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-18 22:33:47 +0100

    18-02  ooga booga solution

--- 18 (RAM Run)/Program.cs
+++ 18 (RAM Run)/Program.cs
@@ -9,20 +9,17 @@ public static class Program
 {
     const int XSize = 71;
     const int YSize = 71;
-    const int SimulateCount = 1024;
     const char Empty = '.';
     const char Rock = '#';
-
     static readonly Point Start = new(0, 0);
-    static readonly Point End = new(XSize - 1, YSize - 1);
 
     [STAThread]
     public static void Main()
     {
-        Solver.Solve<string, long>(Run, true);
+        Solver.Solve<string, string>(Run, true);
     }
 
-    private static long Run(string text)
+    private static string Run(string text)
     {
         var matches = Regex.Matches(text, @"(?<x>\d+),(?<y>\d+)");
 
@@ -31,33 +28,41 @@ public static class Program
 
         var grid = Grid.UniformOfSize(XSize, YSize, Empty);
 
-        for (int i = 0; i < SimulateCount; i++)
+        for (int bruteForceSmileyFace = 0; bruteForceSmileyFace < rocks.Count; bruteForceSmileyFace++)
         {
-            grid.SetValue(rocks[i], Rock);
-        }
-
-        int[,] lowestVisited = new int[XSize, YSize];
-        Span<int> span = MemoryMarshal.CreateSpan(ref lowestVisited[0, 0], XSize * YSize);
-        span.Fill(int.MaxValue);
-
-        List<(Point Point, int Cost)> currentLayer = [(Start, 0)];
-
-        while (currentLayer.Count > 0)
-        {
-            List<(Point, int Cost)> nextLayer = [];
-            foreach (var node in currentLayer)
+            for (int i = 0; i < bruteForceSmileyFace; i++)
             {
-
-                if (lowestVisited[node.Point.X, node.Point.Y] > node.Cost)
-                {
-                    lowestVisited[node.Point.X, node.Point.Y] = node.Cost;
-                    nextLayer.AddRange(grid.Expand(node.Point, Grid.CardinalDirections)
-                        .Where(x => !Equals(grid.GetValue(x), Rock))
-                        .Select(x => (x, node.Cost + 1)));
-                }
+                grid.SetValue(rocks[i], Rock);
             }
-            currentLayer = nextLayer;
+
+            int[,] lowestVisited = new int[XSize, YSize];
+            Span<int> span = MemoryMarshal.CreateSpan(ref lowestVisited[0, 0], XSize * YSize);
+            span.Fill(int.MaxValue);
+
+            List<(Point Point, int Cost)> currentLayer = [(Start, 0)];
+
+            while (currentLayer.Count > 0)
+            {
+                List<(Point, int Cost)> nextLayer = [];
+                foreach (var node in currentLayer)
+                {
+
+                    if (lowestVisited[node.Point.X, node.Point.Y] > node.Cost)
+                    {
+                        lowestVisited[node.Point.X, node.Point.Y] = node.Cost;
+                        nextLayer.AddRange(grid.Expand(node.Point, Grid.CardinalDirections)
+                            .Where(x => !Equals(grid.GetValue(x), Rock))
+                            .Select(x => (x, node.Cost + 1)));
+                    }
+                }
+                currentLayer = nextLayer;
+            }
+            if (lowestVisited[XSize - 1, YSize - 1] == int.MaxValue)
+            {
+                var borked = rocks[bruteForceSmileyFace - 1];
+                return $"{borked.X},{borked.Y}";
+            };
         }
-        return lowestVisited[XSize - 1, YSize - 1];
+        throw new Exception();
     }
 }
