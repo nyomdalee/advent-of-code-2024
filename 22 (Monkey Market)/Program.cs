fceb5035d4b30af89807beec62925fa700bf7a3e
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-22 17:42:04 +0100

    22-02 solution

--- 22 (Monkey Market)/Program.cs
+++ 22 (Monkey Market)/Program.cs
@@ -17,40 +17,66 @@ public static class Program
     {
         var secrets = lines.Select(int.Parse).ToList();
 
+        var histories = secrets.ConvertAll(IterateSecret);
+        var transformed = histories.ConvertAll(DictifyHistory);
 
-        return secrets.Sum(IterateSecret);
+        var sequences = transformed.SelectMany(x => x).Select(x => x.Key).Distinct().ToList();
+
+        return sequences.ConvertAll(x => GetMaximumSequenceValue(x, transformed)).Max();
     }
 
-    private static long IterateSecret(int start)
+    private static long GetMaximumSequenceValue(string combination, List<Dictionary<string, int>> histories)
+    {
+        long sum = 0;
+        foreach (var history in histories)
+        {
+            if (history.TryGetValue(combination, out var result)) ;
+            {
+                sum += result;
+            }
+        }
+        return sum;
+    }
+
+    private static List<(int Cost, int Change)> IterateSecret(int start)
     {
         long secret = start;
-
+        List<(int Cost, int Change)> history = [((int)secret % 10, int.MaxValue)];
         for (int i = 0; i < iterations; i++)
         {
             secret = CalculateNext(secret);
+            var value = (int)secret % 10;
+            history.Add((value, value - history[i].Cost));
         }
-        return secret;
+        return history;
+    }
+
+    private static Dictionary<string, int> DictifyHistory(List<(int Cost, int Change)> history)
+    {
+        Dictionary<string, int> dict = [];
+
+        for (int i = 4; i <= iterations; i++)
+        {
+            string historySequence = $"{history[i - 3].Change},{history[i - 2].Change},{history[i - 1].Change},{history[i].Change}";
+            dict.TryAdd(historySequence, history[i].Cost);
+        }
+        return dict;
     }
 
     private static long CalculateNext(long secret)
     {
-        var yep = secret;
+        var newSecret = secret;
 
-        // 1
-        long times64 = yep * 64;
-        yep = yep ^ times64;
-        yep = yep % modulo;
+        long times64 = newSecret * 64;
+        newSecret ^= times64;
+        newSecret %= modulo;
 
-        // 2
-        long by32 = yep / 32;
-        yep = yep ^ by32;
-        yep = yep % modulo;
+        long by32 = newSecret / 32;
+        newSecret ^= by32;
+        newSecret %= modulo;
 
-        // 3
-        long times2024 = yep * 2048;
-        yep = yep ^ times2024;
-        yep = yep % modulo;
-
-        return yep;
+        long times2024 = newSecret * 2048;
+        newSecret ^= times2024;
+        return newSecret % modulo;
     }
 }
