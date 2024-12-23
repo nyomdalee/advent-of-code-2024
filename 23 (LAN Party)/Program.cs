ea61a665df00ee239d0513928e07bc4025195fbd
nyomdalee <139174287+nyomdalee@users.noreply.github.com>
2024-12-23 18:40:50 +0100

    23-02 brute force solution, graphs? whats that

--- 23 (LAN Party)/Program.cs
+++ 23 (LAN Party)/Program.cs
@@ -4,14 +4,13 @@ namespace TwentyThree;
 
 public static class Program
 {
-
     [STAThread]
     public static void Main()
     {
-        Solver.Solve<string[], long>(Run);
+        Solver.Solve<string[], string>(Run);
     }
 
-    public static long Run(string[] lines)
+    public static string Run(string[] lines)
     {
         var cons = lines.Select(x =>
         {
@@ -24,17 +23,27 @@ public static class Program
             .OrderBy(x => x.Key)
             .ToLookup(x => x.Key, x => x.Select(x => x.Second));
 
-        List<Connection> check = [];
+        List<string> biggestSet = [];
         foreach (var node in lookup)
         {
-            var pairs = GeneratePairs(node.SelectMany(x => x), node.Key);
-            check.AddRange(pairs);
+            var subsets = GenerateSubsets([.. node.SelectMany(x => x), node.Key]);
+            foreach (var set in subsets)
+            {
+                if (set.Count > biggestSet.Count)
+                {
+                    var pairs = GeneratePairs(set);
+                    if (pairs.All(x => cons.Contains(x)))
+                    {
+                        biggestSet = set;
+                    }
+                }
+            }
         }
 
-        return check.Count(x => cons.Contains(x));
+        return string.Join(",", biggestSet.Order());
     }
 
-    static List<Connection> GeneratePairs(IEnumerable<string> list, string start)
+    static List<Connection> GeneratePairs(IEnumerable<string> list)
     {
         var array = list.Order().ToArray();
         List<Connection> pairs = [];
@@ -43,14 +52,35 @@ public static class Program
         {
             for (int j = i + 1; j < array.Length; j++)
             {
-                if (start.StartsWith('t') || array[i].StartsWith('t') || array[j].StartsWith('t'))
-                {
-                    pairs.Add(new(array[i], array[j]));
-                }
+                pairs.Add(new(array[i], array[j]));
             }
         }
 
         return pairs;
+    }
+
+    static List<List<string>> GenerateSubsets(string[] array)
+    {
+        List<List<string>> result = new List<List<string>>();
+        GenerateSubsetsRecursive(array, 0, new List<string>(), result);
+        return result;
+    }
+
+    static void GenerateSubsetsRecursive(string[] array, int index, List<string> current, List<List<string>> result)
+    {
+        if (current.Count > 0)
+        {
+            result.Add(new List<string>(current));
+        }
+
+        for (int i = index; i < array.Length; i++)
+        {
+            current.Add(array[i]);
+
+            GenerateSubsetsRecursive(array, i + 1, current, result);
+
+            current.RemoveAt(current.Count - 1);
+        }
     }
 
     record Connection(string First, string Second);
